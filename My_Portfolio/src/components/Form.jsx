1ea95d533ad72a89af14cff5ffeb76a9c1e857d3
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-07 22:28:24 +0530

    feat/emailworking

--- My_Portfolio/src/components/Form.jsx
+++ My_Portfolio/src/components/Form.jsx
@@ -1,28 +1,36 @@
 import "./FormStyle.css";
 
+import React, { useRef } from "react";
 
-import React from 'react'
+const Form = ({ sendEmail }) => {
+  const formRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formRef.current); //just to check incase of error
+    sendEmail(formRef.current);
+    formRef.current.reset(); // to reset the form after sending mail
+  };
 
-const Form = () => {
+
   return (
     <div className="form">
-      <form>
+      <form ref={formRef} onSubmit={handleSubmit}>
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" name="user_name"></input>
 
         <label>Email</label>
-        <input type="email"></input>
+        <input type="email" name="user_email"></input>
 
         <label>Subject </label>
-        <input type="text"></input>
+        <input type="text" name ="subject"></input>
 
         <label>Message</label>
-        <textarea rows="7" placeholder="What's on your mind?"/>
+        <textarea rows="7" placeholder="What's on your mind?" name="message" />
 
-        <button className="button">Submit</button>
+        <button className="button" type ="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- My_Portfolio/src/routes/Contact.jsx
+++ My_Portfolio/src/routes/Contact.jsx
@@ -1,17 +1,57 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero2 from "../components/Hero2";
 import Form from "../components/Form";
-const Contact = () =>{
-  return(
+import React, { useRef ,useState} from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const [showNotification, setShowNotification] = useState(false);
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    
+    emailjs
+      .sendForm(
+        
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        "form",//class of FORM
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!,email sent");
+          setShowNotification(true);
+          setTimeout(()=>{
+            setShowNotification(false);
+          },3000) // set timeout for 3sec
+        },
+        (error) => {
+          console.log("Couldn't send mail")
+          console.log(error);
+          
+        }
+      );
+  };
+  return (
     <>
-    <Navbar/>
-    <Hero2 heading="Contact ME!" text="What's on your Mind?" />
-    <Form/>
-    <Footer/>
+      <Navbar />
+      <Hero2 heading="Contact ME!" text="What's on your Mind?" />
+      <Form sendEmail={sendEmail} />
+      {showNotification &&(<div className="notification" style={{ position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        padding: "20px",
+        backgroundColor: "rgba(0, 0, 0, 0.8)", // Dark background color
+        color: "white", // Text color
+        borderRadius: "5px",
+        boxShadow: "0 0 10px rgba(255, 255, 255, 0.3)", // Box shadow for depth
+        zIndex: "9999",}}>Email Sent Succesfully</div>)};  
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Contact;
