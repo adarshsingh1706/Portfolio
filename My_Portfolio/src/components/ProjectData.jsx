528be0a0d698bbc3958ef1be6fb8ac343c045621
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-07-05 15:53:46 +0530

    projects update

--- My_Portfolio/src/components/ProjectData.jsx
+++ My_Portfolio/src/components/ProjectData.jsx
@@ -1,37 +1,32 @@
-import calculator from "../assets/calculator.jpg";
-import currency from "../assets/currency.jpg";
 
+import auth from "../assets/auth.jpg";
+import chat from "../assets/chat.jpg";
 
-
-
-const ProjectData=[
+const ProjectData = [
   {
-    imgsrc:currency,
-    title:"Currency Convertor",
-    text:" A Dynamic Currency Converter: Harnessing the Power of APIs and Custom Hooks for Real-Time Exchange Rates!",
-    open:"https://currency-converter-alpha-umber.vercel.app/",
-    source:"https://github.com/adarshsingh1706/CurrencyConverter/tree/main/Currency%20Converter"
-
+    imgsrc: auth,
+    title: "Authentication",
+    text: " This project showcases a robust authentication system with user registration, encrypted password storage in MongoDB Atlas, and secure JWT-based access via HTTP-only cookies. The React frontend interacts with the backend API using axios for efficient and secure authentication.",
+    open: "https://authh-wheat.vercel.app/",
+    source: "https://github.com/adarshsingh1706/Auth",
   },
 
   {
-    imgsrc:"https://images.unsplash.com/photo-1582139329536-e7284fece509?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title:"Password Generator",
-    text:"This React project is a Password Generator app that dynamically creates secure passwords based on user preferences.",
-    open:"https://password-generator-sooty-nine.vercel.app/",
-    source:"https://github.com/adarshsingh1706/React-js/tree/main/Projects/passwordGenerator"
-
+    imgsrc: chat,
+    title: "Chat Here",
+    text: "Developed a real-time chat application using Node.js, Express.js, and Socket.io to facilitate bi-directional communication via websockets. Implemented features include message broadcasting and dynamic message updates. The application ensures efficient and seamless real-time messaging between connected clients.",
+    open: "https://chatapp-68kn.onrender.com/",
+    source: "https://github.com/adarshsingh1706/chatApp",
   },
-
   {
-    imgsrc:calculator,
-    title:"Sleek Calculator",
-    text:"Built a sleek Calculator using #ReactJS! 🚀Deployed via Vercel, it leverages React's props and state for a dynamic user experience.",
-    open:"https://react-js-teal.vercel.app/",
-    source:"https://github.com/adarshsingh1706/React-js/tree/main/Projects/Calculator_version2/Calculator"
-
-  }
-]
-
+    imgsrc:
+      "https://images.unsplash.com/photo-1582139329536-e7284fece509?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Password Generator",
+    text: "This React project is a Password Generator app that dynamically creates secure passwords based on user preferences.",
+    open: "https://password-generator-sooty-nine.vercel.app/",
+    source:
+      "https://github.com/adarshsingh1706/React-js/tree/main/Projects/passwordGenerator",
+  },
+];
 
 export default ProjectData;
